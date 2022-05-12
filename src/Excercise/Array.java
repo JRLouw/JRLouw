b373914a21263e6d48e1a9e45b3a0d2c6dcf0630
Array test <jacorlouw@gmail.com>
2022-05-12 20:04:57 +0200

    https://github.com/JRLouw/Java.git

---
+++ src/Excercise/Array.java
@@ -0,0 +1,9 @@
+import Excercise.variablesCLass;
+
+public class Array {
+    public static void main(String[] args) {
+        // write your code here
+        variablesCLass.rent=9000;
+        System.out.println("average rent is" +" " + variablesCLass.rent+" "+ "in"+" "+ variablesCLass.location);
+    }
+}
---
+++ src/Excercise/variablesCLass.java
@@ -0,0 +1,6 @@
+package Excercise;
+public class variablesCLass {
+    public static double rent = 9000;
+    public static String location ="Midrand";
+
+}
